db8d4b4d7ba442a9566b34132cece565ac0184b9
AmauryPM <amaury.matsumura@gmail.com>
2022-10-04 13:09:41 -0500

    Commit inicial

---
+++ app/src/main/java/com/amaurypm/ciclodevida/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.amaurypm.ciclodevida
+
+import android.content.Intent
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mp: MediaPlayer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("INFORMACION", "onCreate()")
+
+        mp = MediaPlayer.create(this, R.raw.zelda)
+        mp.start()
+
+    }
+
+    fun click(v: View){
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("INFORMACION", "onStart()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("INFORMACION", "onReStart()")
+        mp.start() 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("INFORMACION", "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("INFORMACION", "onPause()")
+        mp.pause()
+
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("INFORMACION", "onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("INFORMACION", "onDestroy()")
+    }
+
+
+}
---
+++ app/src/main/java/com/amaurypm/ciclodevida/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.amaurypm.ciclodevida
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+    fun click(view: View){
+        val intent = Intent(this, MainActivity3::class.java)
+        startActivity(intent)
+    }
+}
